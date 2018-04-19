af9828b427b97ec1369c4352517812970f2b6427
Barthelemy <barthelemy.ehui@gmail.com>
2018-04-19 14:28:14 +0200

    setting up database

--- app/repositories/UserRepository.php
+++ app/repositories/UserRepository.php
@@ -3,12 +3,21 @@
 namespace App\Repositories;
 
 
-class UserRepository implements IRepository
+class UserRepository extends Repository implements IRepository
 {
     
     public function getAll()
     {
-        // TODO: Implement getAll() method.
+        //$this->pdo->query('SELECT * FROM users');
+        $virtualUser1 = new stdClass();
+        $virtualUser1->name = 'alex';
+        $virtualUser1->email = 'alex@gmail.com';
+
+        $virtualUser2 = new stdClass();
+        $virtualUser2->name = 'bose';
+        $virtualUser2->email = 'bose@gmail.com';
+        
+        return [$virtualUser1, $virtualUser2];
     }
     
     public function getById($id)
