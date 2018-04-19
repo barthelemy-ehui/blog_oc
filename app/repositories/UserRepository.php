70630865550477cb7ead3853ca776724dc394cf4
Barthelemy <barthelemy.ehui@gmail.com>
2018-04-19 13:16:10 +0200

    add repository and model layer

---
+++ app/repositories/UserRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories;
+
+
+class UserRepository extends Repository
+{
+    
+    public function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+    
+    public function getById($id)
+    {
+        // TODO: Implement getById() method.
+    }
+}
