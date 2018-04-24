b9c956fc514ff38fe6ca623e0e97d3fffdb71442
Barthelemy <barthelemy.ehui@gmail.com>
2018-04-24 14:20:57 +0200

    add auth

--- public/index.php
+++ public/index.php
@@ -1,7 +1,3 @@
 <?php
 
-<<<<<<< HEAD
-require_once "../vendor/autoload.php";
-=======
-require_once '../app/bootstrap.php';
->>>>>>> 9972880... add boostrap file
+require_once __DIR__.'/../app/bootstrap.php';
