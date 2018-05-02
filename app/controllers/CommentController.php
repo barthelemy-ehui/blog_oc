74c7a1c693d6b0bad3a2c0dd44039dc556118c37
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-02 16:42:16 +0200

    add post and comment controller structure

---
+++ app/controllers/CommentController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controllers;
+
+class CommentController extends Controller
+{
+    public function index(){
+    
+    }
+    
+    public function add()
+    {
+    
+    }
+    
+    public function update($id){
+    
+    }
+    
+    public function delete($id){
+    
+    }
+    
+    public function getCommentById($id){
+        
+    }
+}
