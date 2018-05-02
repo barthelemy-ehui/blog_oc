da46376816fe18c1f505884eaf655137da24c3e1
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-02 16:35:27 +0200

    add Post and Comment repository structure

---
+++ app/repositories/CommentRepository.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Repositories;
+
+
+class CommentRepository extends Repository implements IRepository
+{
+    
+    public function getAll()
+    {
+    
+    }
+    
+    public function getById($id)
+    {
+    
+    }
+    
+    public function updateComment(){
+    
+    }
+    
+    public function insertNewComment(){
+    
+    }
+    
+    public function deleteCommentById($id){
+    
+    }
+}
---
+++ app/repositories/PostRepository.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Repositories;
+
+
+class PostRepository extends Repository implements IRepository
+{
+    
+    public function getAll()
+    {
+    
+    }
+    
+    public function getById($id)
+    {
+    
+    }
+    
+    public function updatePost(){
+    
+    }
+    
+    public function insertNewPost(){
+    
+    }
+    
+    public function deletePostById($id){
+    
+    }
+}
