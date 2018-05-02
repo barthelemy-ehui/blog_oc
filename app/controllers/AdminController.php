77df889fd9f0859d8d5c61fadfc547344ec6481a
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-02 21:16:28 +0200

    add correct annotation on admin

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -4,11 +4,16 @@ namespace App\controllers;
 
 class AdminController extends Controller
 {
+    /**
+     * http_method=get
+     * auth=admin
+     */
     public function index() {
         echo $this->app->load('twig')->render('admin/index.twig');
     }
     
     /**
+     * http_method=get
      * auth=admin
      */
     public function users(){
