d6e71b955eccb2bde6ca325a65867d85e70c9441
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-08 22:11:49 +0200

    update with add and edit

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\Post;
+
 class PostController extends Controller
 {
     /**
@@ -11,23 +13,64 @@ class PostController extends Controller
         echo $this->app->load('twig')->render('admin/post/index.twig');
     }
     
+    /**
+     * http_method=get
+     * auth=admin
+     */
     public function add(){
+        echo $this->app->load('twig')->render('admin/post/add.twig',[
+            'Post' => Post::class
+        ]);
+    }
     
+    public function store(){
+        
+        // valider
+        
+        // stocker dans la base
+        
+        // redirection vers index
+    }
+    
+    /**
+     * http_method=get
+     * auth=admin
+     */
+    public function edit($slug){
+        $datas = [
+            'slug' => '',
+            'title' => '',
+            'description' => '',
+            'content' => '',
+            'status' => '',
+            'publish_at' => ''
+        ];
+        
+        echo $this->app->load('twig')->render('admin/post/edit.twig', [
+            'datas' => $datas,
+            'Post' => Post::class
+        ]);
     }
     
     public function update(){
-    
+        // valider
+        
+        //stocker dans la base
+        
+        //redirection vers index
     }
     
-    public function delete(){
-    
+    public function delete($id){
+        // supprimer
+        
+        // redirection vers index
     }
     
     public function getPostBySlug($slug){
     
     }
     
-    public function getPostById($id){
+    public function getPostById($id) {
     
     }
 }
