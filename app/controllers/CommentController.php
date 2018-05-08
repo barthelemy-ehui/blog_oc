d6809ee854dc91f99e71ef68deec8619343c9596
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-08 22:09:21 +0200

    remove add method from CommentController

--- app/controllers/CommentController.php
+++ app/controllers/CommentController.php
@@ -7,21 +7,32 @@ class CommentController extends Controller
      * http_method=get
      * auth=admin
      */
-    public function index(){
+    public function index() {
         echo $this->app->load('twig')->render('admin/comment/index.twig');
     }
     
-    public function add()
-    {
+    public function store(){
+        // valider
     
+        // stocker dans la base
+    
+        //redirection vers index
     }
+    
     
     public function update($id){
+        // valider
     
+        //stocker dans la base
+    
+        //redirection vers index
     }
     
-    public function delete($id){
-    
+    public function delete($id) {
+        
+        // supprimer
+        
+        // redirection vers index
     }
     
     public function getCommentById($id){
