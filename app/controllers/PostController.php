c81b4f9f387ee101e5043b6db74fd6f2cebf9682
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-10 21:20:26 +0200

    update postController

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -1,7 +1,11 @@
 <?php
 namespace App\Controllers;
 
+use App\Auths\Auth;
+use App\Exceptions\NaNException;
 use App\Models\Post;
+use App\Models\User;
+use App\Validation\Validator;
 
 class PostController extends Controller
 {
@@ -10,7 +14,14 @@ class PostController extends Controller
      * auth=admin
      */
     public function index(){
-        echo $this->app->load('twig')->render('admin/post/index.twig');
+        
+        $posts = $this->app->load('repoManager')
+            ->getInstance('PostRepository')
+            ->getAll();
+
+        echo $this->app->load('twig')->render('admin/post/index.twig', [
+            'posts' => $posts
+        ]);
     }
     
     /**
@@ -18,59 +29,164 @@ class PostController extends Controller
      * auth=admin
      */
     public function add(){
+        
+        $errors = [];
+        $session = $this->app->load('session');
+        if($session->has(Validator::class)) {
+            $errors = $session->get(Validator::class);
+            $session->clear(Validator::class);
+        }
+        
         echo $this->app->load('twig')->render('admin/post/add.twig',[
-            'Post' => Post::class
+            'Post' => Post::class,
+            'errors' => $errors,
         ]);
     }
     
+    /**
+     * http_method=post
+     * auth=admin
+     */
     public function store(){
         
-        // valider
+        $validator = new Validator();
+        $validator->addRule([
+            'slug' => Validator::REQUIRED,
+            'title' => Validator::REQUIRED,
+            'description' => Validator::REQUIRED,
+            'content' => Validator::REQUIRED,
+            'status' => Validator::REQUIRED,
+            'publish_at' => Validator::REQUIRED,
+        ]);
         
-        // stocker dans la base
+        $data = $validator->validate();
+        $errors = $validator->getErrors();
+        if(count($errors['errors'])>0){
+            $this->app->load('session')->set(Validator::class, $errors);
+            $this->redirect('/admin/posts/add');
+            return;
+        }
         
-        // redirection vers index
+        /** @var User $user */
+        $user = $this->app->load('session')->get(Auth::UserAuthentifiedKeySession);
+        
+        $data = array_merge($data, [
+            'author_id' => $user->getId()
+        ]);
+        
+        $data['slug'] =  str_replace(' ','-', $data['slug']);
+        
+        $this->app->load('repoManager')->getInstance('PostRepository')
+            ->insertNewPost($data);
+        
+        $this->redirect('/admin/posts/index');
     }
     
     /**
      * http_method=get
      * auth=admin
      */
-    public function edit($slug){
-        $datas = [
-            'slug' => '',
-            'title' => '',
-            'description' => '',
-            'content' => '',
-            'status' => '',
-            'publish_at' => ''
-        ];
+    public function edit($id){
+    
+        if(!(int) $id[0]){
+            throw new NaNException();
+        }
+
+        $errors = [];
+        $session = $this->app->load('session');
+        if($session->has(Validator::class)) {
+            $errors = $session->get(Validator::class);
+            $session->clear(Validator::class);
+        }
+        
+        $post = $this->app->load('repoManager')
+            ->getInstance('PostRepository')
+            ->getById($id[0]);
         
         echo $this->app->load('twig')->render('admin/post/edit.twig', [
-            'datas' => $datas,
-            'Post' => Post::class
+            'post' => $post,
+            'Post' => Post::class,
+            'errors' => $errors
         ]);
     }
     
-    public function update(){
-        // valider
+    /**
+     * http_method=post
+     * auth=admin
+     */
+    public function update() {
         
-        //stocker dans la base
+        $validator = new Validator();
+        $validator->addRule([
+            'slug' => Validator::REQUIRED,
+            'title' => Validator::REQUIRED,
+            'description' => Validator::REQUIRED,
+            'content' => Validator::REQUIRED,
+            'status' => Validator::REQUIRED,
+            'publish_at' => Validator::REQUIRED,
+            'id' => Validator::REQUIRED
+        ]);
         
-        //redirection vers index
+        $data = $validator->validate();
+        $errors = $validator->getErrors();
+        if( count($errors['errors'])>0 ) {
+            $this->app->load('session')->set(Validator::class, $errors);
+            $this->redirect('/admin/posts/edit/' . $data['id']);
+            return;
+        }
+        
+        $this->app->load('repoManager')
+            ->getInstance('PostRepository')
+            ->updatePost($data);
+        
+        $this->redirect('/admin/posts/index');
     }
     
+    /**
+     * http_method=get
+     * auth=admin
+     */
     public function delete($id){
-        // supprimer
+    
+        if(!(int) $id[0]){
+            throw new NaNException();
+        }
+    
+        $this->app->load('repoManager')
+            ->getInstance('PostRepository')
+            ->deletePostById($id[0]);
         
-        // redirection vers index
+        $this->redirect('/admin/posts/index');
     }
     
-    public function getPostBySlug($slug){
+    /**
+     * http_method=get
+     */
+    public function getPostBySlug($slug) {
     
+        $errors = [];
+        $session = $this->app->load('session');
+        if($session->has(Validator::class)) {
+            $errors = $session->get(Validator::class);
+            $session->clear(Validator::class);
+        }
+        
+        $post =  $this->app->load('repoManager')
+            ->getInstance('PostRepository')
+            ->getById($slug);
+        
+        //todo: à mettre fichier qui permettra d'afficher l'article
+        echo $this->app->load('twig')->render('',[
+            'post' => $post,
+            'errors' => $errors
+        ]);
     }
     
     public function getPostById($id) {
-    
+        $post =  $this->app->load('repoManager')
+            ->getInstance('PostRepository')
+            ->getById($id);
+        
+        // todo: si je souhaite voir l'article complet avec les commentaires, à faire en dernier s'il n'ya pas d'urgence
     }
 }
