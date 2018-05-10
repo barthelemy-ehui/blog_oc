7df95777a975a38c38a617ae6df3dd411ede4080
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-10 21:18:54 +0200

    remove comments and extend Exception

--- app/exceptions/NaNException.php
+++ app/exceptions/NaNException.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bart
- * Date: 10/05/2018
- * Time: 20:28
- */
-
 namespace App\Exceptions;
 
 
-class NaNException
+class NaNException extends \Exception
 {
     
 }
