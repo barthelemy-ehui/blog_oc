7759197c1e074687628e719c0d8441cb83fc3c50
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-16 22:31:07 +0200

    update Register Controller

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -2,6 +2,8 @@
 namespace App\Controllers;
 
 use App\Auths\Auth;
+use App\Exceptions\NaNException;
+use App\Models\User;
 use App\Validation\Validator;
 
 class RegisterController extends Controller
@@ -53,6 +55,88 @@ class RegisterController extends Controller
             ->insertNewUser($data);
         
         header('Location: /admin/users');
+    }
+    
+    
+    /**
+     * http_method=get
+     * auth=admin
+     */
+    public function edit($id){
+        
+        if(!(int) $id[0]) {
+           throw new NaNException();
+        }
+        
+        $errors = [];
+        $session = $this->app->load('session');
+        if($session->has(Validator::class)) {
+            $errors = $session->get(Validator::class);
+            $session->clear(Validator::class);
+        }
+        
+        $user = $this->app->load('repoManager')
+            ->getInstance('UserRepository')
+            ->getById($id[0]);
+        
+        
+        echo $this->app->load('twig')->render('admin/user/edit.twig',[
+            'user' => $user,
+            'User' => User::class,
+            'errors' => $errors
+        ]);
+    }
+    
+    /**
+     * http_method=post
+     * auth=admin
+     */
+    public function update(){
+        
+        $validator = new Validator();
+        $validator->addPasswordToCompare('passwordConfirm');
+        $validator->addRule([
+            'firstname' => Validator::REQUIRED,
+            'lastname' => Validator::REQUIRED,
+            'email' => Validator::REQUIRED_EMAIL,
+            'password' => Validator::REQUIRED_PASSWORD_COMPARE,
+            'id' => Validator::REQUIRED
+        ]);
+    
+    
+        $data = $validator->validate();
+        $errors = $validator->getErrors();
+        
+        if($errors['errors'] && $errors['datas']){
+            $this->app->load('session')->set(Validator::class, $errors);
+            $this->redirect('/admin/register/edit/' . $errors['datas']['id']);
+            return;
+        }
+    
+        
+        $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
+    
+        $this->app->load('repoManager')
+            ->getInstance('UserRepository')
+            ->updateUser($data);
+        
+        $this->redirect('/admin/users');
+    }
+    
+    /**
+     * http_method=get
+     * auth=admin
+     */
+    public function deleteUserById($id) {
+        
+        if(!(int) $id[0]){
+            throw new NaNException();
+        }
+    
+       $this->app->load('repoManager')
+           ->getInstance('UserRepository')
+           ->deleteUserById($id[0]);
+       $this->redirect('/admin/users');
     }
     
     /**
