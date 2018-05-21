7903df5264262a082e181e5b53d242ab9b07d7f2
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-21 23:06:15 +0200

    update front content

--- app/repositories/PostRepository.php
+++ app/repositories/PostRepository.php
@@ -22,7 +22,7 @@ class PostRepository extends Repository implements IRepository
     public function getAllByPagination($offset, $limit)
     {
         
-        $sqlStmt = 'SELECT * FROM posts WHERE status = :status LIMIT :offset, :limit';
+        $sqlStmt = 'SELECT * FROM posts WHERE status = :status AND publish_at<=NOW() ORDER BY update_at DESC LIMIT :offset, :limit';
         $stmt = $this->pdo->prepare($sqlStmt);
         $stmt->bindValue(':offset',$offset*$limit,\PDO::PARAM_INT);
         $stmt->bindValue(':limit',$limit,\PDO::PARAM_INT);
@@ -37,7 +37,7 @@ class PostRepository extends Repository implements IRepository
     
     public function countAll(){
         
-        $sqlStmt = 'SELECT count(*) FROM posts WHERE status = :status';
+        $sqlStmt = 'SELECT count(*) FROM posts WHERE status = :status AND publish_at<=NOW()';
         $stmt = $this->pdo->prepare($sqlStmt);
         $stmt->bindValue(':status', POST::PUBLISHED, \PDO::PARAM_STR);
         $stmt->execute();
@@ -95,11 +95,12 @@ BEGIN;
     public function insertNewPost($data) {
         
         $data = array_merge($data,[
-            'create_at' => (new \DateTime('now'))->format('Y-m-d H:i:s')
+            'create_at' => (new \DateTime('now'))->format('Y-m-d H:i:s'),
+            'update_at' => (new \DateTime('now'))->format('Y-m-d H:i:s')
         ]);
         
         $sqlStmt = <<<BEGIN
-          INSERT INTO posts (slug,title,description,content,status,author_id,publish_at,create_at) VALUES (:slug,:title,:description,:content,:status,:author_id,:publish_at,:create_at);
+          INSERT INTO posts (slug,title,description,content,status,author_id,publish_at,create_at,update_at) VALUES (:slug,:title,:description,:content,:status,:author_id,:publish_at,:create_at,:update_at);
 BEGIN;
         
         $stmt = $this->pdo->prepare($sqlStmt);
@@ -118,5 +119,27 @@ BEGIN;
         $stmt->execute([
             ':id' => $id
         ]);
+    }
+    
+    function getCount(){
+        $sqlStmt = 'SELECT count(*) FROM posts';
+        $stmt = $this->pdo->prepare($sqlStmt);
+        $stmt->execute();
+    
+        return $stmt->fetch(\PDO::FETCH_NUM)[0];
+    }
+    
+    function getTheTreeLatestPosts(){
+        
+        $sqlStmt = 'SELECT * FROM posts WHERE status = :status AND publish_at<=NOW() ORDER BY update_at DESC LIMIT :limit';
+        $stmt = $this->pdo->prepare($sqlStmt);
+        $stmt->bindValue(':limit',3,\PDO::PARAM_INT);
+        $stmt->bindValue(':status', POST::PUBLISHED,\PDO::PARAM_STR);
+        $stmt->execute();
+    
+        return $stmt->fetchAll(
+            \PDO::FETCH_CLASS,
+            Post::class
+        );
     }
 }
--- public/assets/front/js/contact_me.js
+++ public/assets/front/js/contact_me.js
@@ -10,7 +10,6 @@ $(function() {
       // get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
@@ -20,11 +19,10 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "/send/email",
         type: "POST",
         data: {
           name: name,
-          phone: phone,
           email: email,
           message: message
         },
@@ -35,7 +33,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Votre message a été envoyé. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -46,7 +44,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Désolé " + firstName + ", il semble que le serveur email ne répond pas. Merci de ré-essayer plus tard!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
