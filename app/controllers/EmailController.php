f9292b9671824d87ab6b91e2c1cd4f747e6c25e9
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-21 23:09:14 +0200

    send email when contacting

--- app/controllers/EmailController.php
+++ app/controllers/EmailController.php
@@ -1,15 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bart
- * Date: 14/05/2018
- * Time: 20:08
- */
-
 namespace App\Controllers;
 
 
-class EmailController
+class EmailController extends Controller
 {
+    public function send()
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $recipient = 'b.ehuinda@gmail.com';
+        $mail_body = $_POST['message'];
+        $subject = 'Email de - ' . $_POST['name'];
+        $header = 'From: '. $name . ' <' . $email . '>\r\n';
     
+        mail($recipient, $subject, $mail_body, $header);
+    }
 }
