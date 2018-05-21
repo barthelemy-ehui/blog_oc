d63426701967c9009f02776a630dfa29b55497a2
Barthelemy <barthelemy.ehui@gmail.com>
2018-05-21 23:08:08 +0200

    add the three latest blog

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,7 +10,12 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $latestThreePosts = $this->app->load('repoManager')
+        ->getInstance('PostRepository')
+        ->getTheTreeLatestPosts();
         
-        echo $this->app->load('twig')->render('front/index.twig');
+        echo $this->app->load('twig')->render('front/index.twig',[
+            'posts' => $latestThreePosts
+        ]);
     }
 }
